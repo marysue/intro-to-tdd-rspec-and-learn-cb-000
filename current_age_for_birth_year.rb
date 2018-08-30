dd36ed4f48d79230cd308799e39a44359ae9fb4c
Mary Lark <mary@marylark.com>
2018-08-30 02:42:19 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,11 @@
 
+<<<<<<< HEAD
 def current_age_for_birth_year(birth_year)
   return (2003 - birth_year)
 end
+=======
+    def current_age_for_birth_year(birth_year)
+      2003 - birth_year
+    end
+
+>>>>>>> 184f36150ad5e2be312db2682f38a10ec54bdfa2
